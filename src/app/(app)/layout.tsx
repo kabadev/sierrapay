9e6e7713201c9d5119f13509c30f4332ea473015
HP <raoulvierra25@gmail.com>
2025-08-12 12:02:24 +0000

    change font

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -9,7 +9,7 @@ export default function Layout({
   return (
     <div className="bg-[url('/bg-win.png')]/  fixed top-0 bottom-0 left-0 right-0  h-screen w-full bg-fixed max-md:bg-opacity-10 bg-no-repeat bg-cover">
       <Navbar />
-      <div className="md:w-[100%] mt-[60px] md:mt-[70px] max-md:mb-[80px] pb-20 ">
+      <div className="md:w-[100%] mt-[60px] md:mt-[70px] max-md:mb-[160px]  ">
         {children}
       </div>
       <Footer />
